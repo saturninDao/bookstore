8713479430ff9995a79c5ad1651be7a35cb35315
Saturnin Dao <saturnindao@gmail.com>
2020-05-23 22:56:06 +0000

    service auth with firebase ok

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor() {}
+  createNewUser(email:string, password: string){
+    return new Promise(
+      (resolve,reject)=>{
+          firebase.auth().createUserWithEmailAndPassword(email,password).then(
+            ()=>{
+              resolve();
+            },
+            (error)=>{
+              reject(error);
+            }
+          );
+      }
+    );
+  }
+  signInUser(email:string, password: string){
+    return new Promise(
+      (resolve,reject)=>{
+        firebase.auth().signInWithEmailAndPassword(email,password).then(
+          ()=>{
+            resolve();
+          },
+          (error)=>{
+            reject(error);
+          }
+        )
+      }
+      );
+  }
+
+  signOut(){
+    firebase.auth().signOut();
+  }
+
 }
