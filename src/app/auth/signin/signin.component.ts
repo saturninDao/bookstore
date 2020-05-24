273ef48bf3c5e90d187f7ab5a77da19c462c5511
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 10:40:49 +0000

    signin ts and html ok

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signInForm: FormGroup;
+  errorMessage: string;
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+    this.signInForm = this.formBuilder.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required,Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit(){
+    const email = this.signInForm.get('email').value;
+    const password  = this.signInForm.get('password').value;
+    this.authService.signInUser(email,password).then(
+      ()=> {
+        this.router.navigate(['/books']);
+      },
+      (error)=> {
+        this.errorMessage = error;
+      }
+    )
   }
 
 }
