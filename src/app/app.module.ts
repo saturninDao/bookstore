2cbd48c22d3a8029b57e87b59074b3ba848c0590
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 12:46:25 +0000

    we change code structure for security issue (hidding google api key in a different class that we put in gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { MyConfig } from './config/config';
 
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent},
@@ -45,7 +46,8 @@ const appRoutes: Routes = [
   providers: [
     AuthService,
     BooksService,
-    AuthGuardService
+    AuthGuardService,
+    MyConfig
   ],
   bootstrap: [AppComponent]
 })
