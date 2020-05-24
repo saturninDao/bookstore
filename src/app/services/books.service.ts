ccc451d0895bb2f1af764ffa6b5604dec89d1671
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 15:53:24 +0000

    books service ok

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class BooksService {
 
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
   constructor() { }
+
+  emitBooks(){
+    this.booksSubject.next(this.books);
+  }
+  saveBooks(){
+    firebase.database().ref('/books').set(this.books);
+  }
+  getBooks(){
+    firebase.database().ref('/books')
+      .on('value',(data)=>{
+        this.books = data.val()?data.val():[];
+        this.emitBooks();
+      })
+  }
+  getSingle(id){
+    return new Promise(
+      (resolve,reject)=>{
+        firebase.database().ref('/books/'+1).once('value').then(
+          (data) => {
+            resolve(data.val());
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+
+  createNewBook(newbook: Book) {
+    this.books.push(newbook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book){
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl)=>{
+        if(bookEl==book){
+          return true;
+        }
+      }
+    )
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
