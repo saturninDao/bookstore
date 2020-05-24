b963f6bd340cf1db4164ebd70ff7a9f9a9ecd4fc
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 15:53:51 +0000

    books lis code

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubscriber: Subscription = new Subscription();
+  constructor(private bookService: BooksService, private router: Router) { }
 
   ngOnInit(): void {
+    this.booksSubscriber = this.bookService.booksSubject.subscribe(
+      (books: Book[])=>{
+        this.books = books;
+      }
+    );
+    this.bookService.emitBooks();
+  }
+
+  onNewBook(){
+    this.router.navigate(['/books','new']);
+  }
+
+  onDeleteBook(book:Book){
+    this.bookService.removeBook(book);
+  }
+
+  onViewBook(id:number){
+    this.router.navigate(['/books','view',id]);
+  }
+
+  ngOnDestroy(){
+    this.booksSubscriber.unsubscribe();
   }
 
 }
