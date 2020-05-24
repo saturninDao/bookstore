33b7a199e0b8f08c4f5ebb661f7788d72cf3b275
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 17:17:41 +0000

    view of a sigle book

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book:Book;
+  constructor(private route:ActivatedRoute,
+              private router:Router,
+              private bookService:BooksService) { }
 
   ngOnInit(): void {
+   // We create empty book wating for the promise of service to avoid errors
+   this.book = new Book('','');
+   //We get the id of the book we are about to look at
+   const id = this.route.snapshot.params['id'];
+   this.bookService.getSingle(+id).then(
+     (book:Book)=>{
+       this.book = book;
+     }
+   );
   }
+
+  onBack(){
+    this.router.navigate(['books']);
+  }
+
+
 
 }
