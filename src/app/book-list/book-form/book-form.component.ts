074a3771a0478c183f2dcee5d08bd651ef99721d
Saturnin Dao <saturnindao@gmail.com>
2020-05-24 17:18:21 +0000

    new book form

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BooksService } from 'src/app/services/books.service';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
 
 @Component({
   selector: 'app-book-form',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private booksService: BooksService,
+              private router:Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+    this.bookForm = this.formBuilder.group({
+      title: ['',Validators.required],
+      author: ['',Validators.required]
+    });
+  }
+
+  onSaveBook(){
+    const title = this.bookForm.get('title').value;
+    const author = this.bookForm.get('author').value;
+    const newBook = new Book(title,author);
+    this.booksService.createNewBook(newBook);
+    this.router.navigate(['books']);
   }
 
 }
